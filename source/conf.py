c7f7f6ff7ca64f20457f6aa602c77db82481890e
yonching <yong.qing.myemail@gmail.com>
2022-07-26 12:05:09 +0800

    sphinx

--- source/conf.py
+++ source/conf.py
@@ -56,6 +56,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+#html_baseurl='https://yonching.github.io/html/'
+#html_theme_path=['https://yonching.github.io/html/']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
